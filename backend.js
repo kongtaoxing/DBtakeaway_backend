55b2595a3f00f8ff7b37a56cb4ca382fdfc0773a
kongtaoxing <xyf8658@163.com>
2023-04-20 09:12:29 +0800

    add async main

--- backend.js
+++ backend.js
@@ -1,25 +1,39 @@
-const {Sequelize}=require('sequelize')
- 
-const sequelize=new Sequelize('orders','root','20281128',{
-    dialect:'mysql',
-    host:'172.24.65.85',
-    port:3306,
-    logging:false,
- 
-})
+const { Sequelize }=require('sequelize')
 
-const dbConnection = async () => {
-    try {
-        await sequelize.authenticate();
-        console.log('mysql Connection successful !!! ');
-        const [results, metadata] = await sequelize.query("SELECT * FROM menu");
-        console.log(results)
-// Results will be an empty array and metadata will contain the number of affected rows.
-    } catch (error) {
-        console.error('Unable to connect to the database:', error);
+const main = async () => {
+ 
+    const sequelize=new Sequelize('orders','root','20281128',{
+        dialect:'mysql',
+        host:'172.24.65.85',
+        port:3306,
+        logging:false,
+    
+    })
+
+    const dbConnection = async () => {
+        try {
+            await sequelize.authenticate();
+            console.log('mysql Connection successful !!! ');
+            const [results, metadata] = await sequelize.query("SELECT * FROM menu");
+            console.log(results)
+    // Results will be an empty array and metadata will contain the number of affected rows.
+        } catch (error) {
+            console.error('Unable to connect to the database:', error);
+        }
+    
     }
- 
+    dbConnection();
 }
-dbConnection()
 
+const runMain = async () => {
+    try {
+        await main();
+        process.exit(0);
+    }
+    catch(e) {
+        console.error(e);
+        process.exit(1);
+    }
+}
 
+runMain();
